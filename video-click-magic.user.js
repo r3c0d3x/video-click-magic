30b988d962445c52c50878f4a29341638fefc96b
r3c0d3x <r3c0d3x@users.noreply.github.com>
2016-10-15 14:19:57 -0400

    Updated version

--- video-click-magic.user.js
+++ video-click-magic.user.js
@@ -3,7 +3,7 @@
 // @name Click to Play/Pause <video>
 // @description Makes it so <video> elements will play/pause when clicking the video, not just the controls. Requested by someone from IRC.
 // @include *
-// @version 1.1
+// @version 1.2
 // @updateURL https://r3c0d3x.github.io/video-click-magic/video-click-magic.user.js
 // ==/UserScript==
 
