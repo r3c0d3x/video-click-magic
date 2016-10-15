cfd4849284a978f1bd91a747313bffa821f647eb
r3c0d3x <r3c0d3x@users.noreply.github.com>
2016-10-15 11:44:37 -0400

    Created video-click-magic.user.js

---
+++ video-click-magic.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @id video-click-magic
+// @name Click to Play/Pause <video>
+// @description Makes it so <video> elements will play/pause when clicking the video, not just the controls. Requested by someone from IRC.
+// @include *
+// @version 1
+// ==/UserScript==
+
+window.addEventListener('load', function() {
+    Array.prototype.forEach.call(document.getElementsByTagName('video'), function(elem) {
+        elem.addEventListener('click', playPause);
+    });
+}, false);
+
+function playPause() {
+	  if (this.paused)
+        this.play();
+    else
+        this.pause();
+}
+
+(new MutationObserver(function(muts) {
+    muts.forEach(function(mut) {
+        mut.addedNodes.forEach(function(elem) {
+            if (elem.tagName === 'VIDEO')
+                elem.addEventListener('click', playPause);
+        });
+    });
+})).observe(document.body, { childList: true });
